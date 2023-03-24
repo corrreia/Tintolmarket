ed113071dfe8825282040289dc954f282acbf0c9
corrreia <tomasc2002@gmail.com>
2023-03-24 21:54:42 +0000

    small fix regarding registering

--- Tintolmarket/src/main/Tintolmarket.java
+++ Tintolmarket/src/main/Tintolmarket.java
@@ -17,33 +17,33 @@ public class Tintolmarket {
         try {
             String clientID, password;
 
-            if (args.length < 2 || args.length > 3){
+            if (args.length < 2 || args.length > 3) {
                 stdIn.close();
                 throw new IncorrectParametersClientException();
             } else {
-                if (args[0].contains(":")){
+                if (args[0].contains(":")) {
 
                     // expected usage: 127.0.0.1:12345 OR localhost:12345
                     String[] serverAddressSplit = args[0].split(":");
                     String serverAddress = serverAddressSplit[0];
                     int port = DEFAULT_PORT;
 
-                    if (serverAddressSplit.length > 1){
+                    if (serverAddressSplit.length > 1) {
                         try {
                             port = Integer.parseInt(serverAddressSplit[1]);
                         } catch (NumberFormatException e) {
                             System.err.println("Cannot parse port number");
                             System.exit(1);
                         }
-                    }else {
+                    } else {
                         System.out.println("No port number specified. Defaulting to " + DEFAULT_PORT);
                     }
 
                     clientID = args[1];
-                    //parse password
-                    if (args.length > 2){
+                    // parse password
+                    if (args.length > 2) {
                         password = args[2];
-                    }else{
+                    } else {
                         System.out.print("No password specified. Please enter password: ");
                         password = stdIn.readLine();
                     }
@@ -72,20 +72,20 @@ public class Tintolmarket {
         try {
 
             int flagFromServer = inStream.readInt();
-            if (flagFromServer == 1){ // user registered
+            if (flagFromServer == 1) { // user registered
                 System.out.println("Logging in...\n");
                 boolean loginSuccessful = (Boolean) inStream.readObject();
-                if (loginSuccessful){
+                if (loginSuccessful) {
                     System.out.println("Welcome back " + clientID + " :)\n");
-                }else{
+                } else {
                     System.out.println("Failed to login" + clientID + " :(\n");
                     System.exit(1);
                 }
             } else {
                 System.out.println("Registering new user...\n");
                 boolean registerSuccessful = (Boolean) inStream.readObject();
-                if(registerSuccessful){
-                    System.out.println("Welcome to TintolMarket" + clientID + " :)\n");
+                if (registerSuccessful) {
+                    startClient(serverAddress, port, clientID, password); // login after registering
                 }
             }
         } catch (ClassNotFoundException e) {
