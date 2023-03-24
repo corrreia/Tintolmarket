37aa2bf704d78761799970d5c2800402cd3f7b29
Miguel Pato <migu3l.pach3co@gmail.com>
2023-03-24 17:58:46 +0000

    objects javadocs done

--- TintolmarketServer/src/objects/User.java
+++ TintolmarketServer/src/objects/User.java
@@ -4,6 +4,17 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.LinkedList;
 
+/**
+ * Class that represents a user in the system.
+ * It contains the name of the user,
+ * the balance of the user,
+ * the wines listed by the user and
+ * the inbox of the user.
+ * 
+ * @author Tomás Correia | fc57102
+ * @author Miguel Pato   | fc56372
+ * @author João Vieira   | fc45677
+ */
 public class User implements Serializable {
     // USERS:
     // name, wines (id of wines, price, quantity), inbox (for messages)
@@ -13,16 +24,32 @@ public class User implements Serializable {
     private LinkedList<WineUser> wines;
     private HashMap<String, String> inbox;
 
+    /**
+     * Constructor for the User class.
+     * 
+     * @param name The name of the user.
+     * @param balance The balance of the user.
+     */
     public User(String name, float balance) {
         this.name = name;
         this.balance = balance;
         this.wines = new LinkedList<WineUser>();
     }
 
+    /**
+     * Adds a wine to the user's list of wines.
+     * 
+     * @param wine  The wine to be added.
+     */
     public void addWineListing(WineUser wine) {
         this.wines.add(wine);
     }
 
+    /**
+     * Removes a wine from the user's list of wines.
+     * 
+     * @param wine  The wine to be removed.
+     */
     public void removeWine(String wine) {
         for (WineUser wineO : this.wines)
             if (wineO.getName().equals(wine)) {
@@ -31,6 +58,12 @@ public class User implements Serializable {
             }
     }
 
+    /**
+     * Gets a wine from the user's list of wines.
+     * 
+     * @param wine  The name of the wine to be retrieved.
+     * @return The wine with the given name.
+     */
     public WineUser getWine(String wine) {
         for (WineUser wineO : this.wines)
             if (wineO.getName().equals(wine))
@@ -38,32 +71,68 @@ public class User implements Serializable {
         return null;
     }
 
+    /**
+     * Gets the name of the user.
+     * 
+     * @return The name of the user.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the balance of the user.
+     * 
+     * @return The balance of the user.
+     */
     public float getBalance() {
         return balance;
     }
 
+    /**
+     * Sets the name of the user.
+     * 
+     * @param name The name of the user.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Sets the balance of the user.
+     * 
+     * @param balance The new balance of the user.
+     */
     public void setBalance(float balance) {
         if (balance < 0)
             throw new IllegalArgumentException("Balance cannot be negative");
         this.balance = balance;
     }
 
+    /**
+     * Gets the list of wines of the user.
+     * 
+     * @return The list of wines of the user.
+     */
     public LinkedList<WineUser> getWines() {
         return wines;
     }
 
+    /**
+     * Adds a message to the user's inbox.
+     * 
+     * @param sender    The sender of the message.
+     * @param message   The message to be added.
+     */
     public void addMessage(String sender, String message) {
         this.inbox.put(sender, message);
     }
 
+    /**
+     * Gets the user's inbox.
+     * 
+     * @return The user's inbox.
+     */
     public HashMap<String, String> readInbox() {
         HashMap<String, String> inbox = this.inbox;
         this.inbox = new HashMap<String, String>();
--- TintolmarketServer/src/objects/WineStore.java
+++ TintolmarketServer/src/objects/WineStore.java
@@ -3,6 +3,15 @@ package objects;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * Class that represents a wine in the store.
+ * It contains the name of the wine,
+ * the image of the wine and the evaluations of the wine. 
+ * 
+ * @author Tomás Correia | fc57102
+ * @author Miguel Pato   | fc56372
+ * @author João Vieira   | fc45677
+ */
 public class WineStore implements Serializable {
 
     private String name;
@@ -10,10 +19,11 @@ public class WineStore implements Serializable {
     private ArrayList<Float> evaluations;
 
     /**
-     *
-     * @param name
-     * @param image
-     * @param evaluations
+     * Constructor for the WineStore class.
+     * 
+     * @param name    The name of the wine.
+     * @param image  The image of the wine.
+     * @param evaluations   The evaluations of the wine.
      */
     public WineStore(String name, String image, ArrayList<Float> evaluations) {
         this.name = name;
@@ -21,20 +31,41 @@ public class WineStore implements Serializable {
         this.evaluations = evaluations;
     }
 
+    /**
+     * Constructor for the WineStore class.
+     * 
+     * @param name    The name of the wine.
+     * @param image  The image of the wine.
+     */
     public WineStore(String name, String image) {
         this.name = name;
         this.image = image;
         this.evaluations = new ArrayList<Float>();
     }
 
+    /**
+     * Adds a new evaluation to the wine.
+     * 
+     * @param evaluation The evaluation to be added.
+     */
     public void newEvaluation(float evaluation) {
         evaluations.add(evaluation);
     }
 
+    /**
+     * Gets the number of evaluations of the wine.
+     * 
+     * @return The number of evaluations of the wine.
+     */
     public int getNrOfEvaluations() {
         return evaluations.size();
     }
 
+    /**
+     * Gets the evaluation of the wine.
+     * 
+     * @return The evaluation of the wine.
+     */
     public float getEvaluation() {
         float sum = 0;
         for (float evaluation : evaluations) {
@@ -46,30 +77,60 @@ public class WineStore implements Serializable {
         return sum / evaluations.size();
     }
 
+    /**
+     * Gets the path of an image of the wine.
+     * 
+     * @return The path to the image of the wine.
+     */
     public String getImage() {
         return image;
     }
 
+    /**
+     * Sets the number of evaluations of the wine.
+     * 
+     * @param nrOfEvaluations The number of evaluations of the wine.
+     */
     public void setNrOfEvaluations(int nrOfEvaluations) {
         this.evaluations = new ArrayList<Float>();
     }
 
+    /**
+     * Sets the evaluation of the wine.
+     * 
+     * @param evaluation The evaluation of the wine.
+     */
     public void setEvaluation(float evaluation) {
         this.evaluations.add(evaluation);
     }
 
+    /**
+     * Sets the path of an image of the wine.
+     * 
+     * @param image The path to the image of the wine.
+     */
     public void setImage(String image) {
         this.image = image;
     }
 
+    /**
+     * Gets the name of the wine.
+     * 
+     * @return The name of the wine.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the wine.
+     * 
+     * @param name The name of the wine.
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    
     @Override
     public String toString() {
         return "Wine [evaluation=" + getEvaluation() + ", image=" + image + ", name=" + name + "]";
